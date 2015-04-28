3858a2c9c4d71a794aa890f573823ce10a4a094a
Anita <anitanikolova@yahoo.com>
2015-04-29 02:57:08 +0300

    php

---
+++ php-mvc-blog/controllers/BaseController.php
@@ -0,0 +1,9 @@
+<?php
+
+abstract class BaseController
+{
+    public function index()
+    {
+        // Implement the default action in the subclasses
+    }
+}
--- php-mvc-blog/index.php
+++ php-mvc-blog/index.php
@@ -1,18 +1,44 @@
 <?php
 require_once('includes/config.php');
 
-$requestParts = explode('/', $_SERVER['REQUEST_URI']);
+$requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$requestParts = explode('/', $requestPath);
 
-$controller = DEFAULT_CONTROLLER;
+$controllerName = DEFAULT_CONTROLLER;
 if (count($requestParts) >= 2 && $requestParts[1] != '') {
-    $controller = $requestParts[1];
+    $controllerName = $requestParts[1];
 }
 
 $action = DEFAULT_ACTION;
 if (count($requestParts) >= 3 && $requestParts[2] != '') {
     $action = $requestParts[2];
 }
+$params = array_splice($requestParts, 3);
 
-var_dump($requestParts);
-var_dump($controller);
-var_dump($action);
+$controllerClassName = ucfirst(strtolower($controllerName)) . 'Controller';
+$controllerFileName = "controllers/" . $controllerClassName . '.php';
+
+// Create controller
+if (class_exists($controllerClassName)) {
+    $controller = new $controllerClassName();
+} else {
+    die("Cannot find controller '$controller' in class '$controllerFileName'");
+}
+
+// Call action
+if (method_exists($controller, $action)) {
+    $controller->{$action}();
+} else {
+    die("Cannot find action '$action' in controller '$controllerClassName'");
+}
+
+function __autoload($class_name)
+{
+    if (file_exists("controllers/$class_name.php")) {
+        include "controllers/$class_name.php";
+    }
+    if (file_exists("models/$class_name.php")) {
+        include "models/$class_name.php";
+    }
+}
+
