22a0a9a8943de7de0227566b1119a05f7c45bc8e
Anita <anitanikolova@yahoo.com>
2015-04-29 01:47:55 +0300

    php

--- php-mvc-blog/index.php
+++ php-mvc-blog/index.php
@@ -1,3 +1,18 @@
 <?php
-echo "I am index.php";
+require_once('includes/config.php');
 
+$requestParts = explode('/', $_SERVER['REQUEST_URI']);
+
+$controller = DEFAULT_CONTROLLER;
+if (count($requestParts) >= 2 && $requestParts[1] != '') {
+    $controller = $requestParts[1];
+}
+
+$action = DEFAULT_ACTION;
+if (count($requestParts) >= 3 && $requestParts[2] != '') {
+    $action = $requestParts[2];
+}
+
+var_dump($requestParts);
+var_dump($controller);
+var_dump($action);
