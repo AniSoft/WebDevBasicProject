b4ac7177797d13046306850cdf2c724d9d5c8d90
Anita <anitanikolova@yahoo.com>
2015-04-30 01:09:03 +0300

    php

--- php-mvc-blog/views/authors/index.php
+++ php-mvc-blog/views/authors/index.php
@@ -1,1 +1,15 @@
+<h1><?= htmlspecialchars($this->title) ?></h1>
 Hello, I am the Authors Index veiw.
+
+<table>
+    <tr>
+        <th>Id</th>
+        <th>name</th>
+    </tr>
+    <?php foreach ($this->authors as $author): ?>
+        <tr>
+            <td><?= htmlspecialchars($author['id']) ?></td>
+            <td><?= htmlspecialchars($author['name']) ?></td>
+        </tr>
+    <?php endforeach ?>
+</table>
