84a9a74c581de6e85762fc8e452b8bdabe5b8306
Anita <anitanikolova@yahoo.com>
2015-05-04 01:34:46 +0300

    php

--- php-mvc-blog/controllers/AccountController.php
+++ php-mvc-blog/controllers/AccountController.php
@@ -4,13 +4,21 @@ class AccountController extends BaseController
 {
     private $db;
 
-    public function onInit(){
-        $this->db=new AccountModel();
+    public function onInit()
+    {
+        $this->db = new AccountModel();
     }
 
     public function register()
     {
-        var_dump($_POST);
+        if($this->isPost){s
+            // Get username
+            // Get password
+            // Register user
+            // Redirect user
+            $this->redirect("books","index");
+        }
+
         $this->renderView(__FUNCTION__);
     }
 
