29d1609a7298596019c1c27771ae540d71981aec
Anita <anitanikolova@yahoo.com>
2015-05-04 00:08:26 +0300

    php

--- php-mvc-blog/controllers/AccountController.php
+++ php-mvc-blog/controllers/AccountController.php
@@ -2,9 +2,16 @@
 
 class AccountController extends BaseController
 {
+    private $db;
+
+    public function onInit(){
+        $this->db=new AccountModel();
+    }
+
     public function register()
     {
-
+        var_dump($_POST);
+        $this->renderView(__FUNCTION__);
     }
 
     public function login()
--- php-mvc-blog/models/AccountModel.php
+++ php-mvc-blog/models/AccountModel.php
@@ -5,10 +5,21 @@ class AccountModel extends BaseModel
     // Method
     public function register($username, $password)
     {
+        $statement = self::$db->prepare("SELECT COUNT(Id) FROM Users WHERE Username=?");
+        $statement->bind_param("s", $username);
+        $statement->execute();
+        $result=$statement->get_result()->fetch_assoc();
+
+        var_dump($result);
+        if($result){
+            return false;
+        }
+
 
     }
 
-    public function login($username, $password){
+    public function login($username, $password)
+    {
 
     }
 }
