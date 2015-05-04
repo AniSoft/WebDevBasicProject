a6cacfff6c9ac7d34b443cf78288de099f6f2526
Anita <anitanikolova@yahoo.com>
2015-05-04 21:23:17 +0300

    php

--- php-mvc-blog/models/BooksModel.php
+++ php-mvc-blog/models/BooksModel.php
@@ -1,1 +1,8 @@
 <?php
+class BookModel extends  BaseModel{
+    public function getAll(){
+        $statement=self::$db->prepare("SELECT title FROM books");
+        $result=$statement->fetch_all();
+        return $result;
+    }
+}
