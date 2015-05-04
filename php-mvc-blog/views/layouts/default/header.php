e5c68cd41b7c6f45234a5209c616bd0929645a2e
Anita <anitanikolova@yahoo.com>
2015-05-04 16:40:52 +0300

    php

--- php-mvc-blog/views/layouts/default/header.php
+++ php-mvc-blog/views/layouts/default/header.php
@@ -14,14 +14,17 @@
     <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/authors">Authors</a></li>
-        <li><a href="/books">Books</a></li>
+        <?php if ($this->isLoggedIn): ?>
+            <li><a href="/books">Books</a></li>
+        <?php endif; ?>
     </ul>
-    <?php if($this->isLoggedIn):?>
+    <?php if ($this->isLoggedIn): ?>
         <div id="logged-in-info">
             <span>Hello, <?php echo $_SESSION['username']; ?></span>
+
             <form action="/account/logout"><input type="submit" value="Logout"/></form>
         </div>
-    <?php endif;?>
+    <?php endif; ?>
 </header>
 
 <?php include('messages.php'); ?>
