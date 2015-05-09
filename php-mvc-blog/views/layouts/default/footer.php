db2b3ade3759eedf215c982ebc9a492c773f4b1b
Anita <anitanikolova@yahoo.com>
2015-05-09 21:14:50 +0300

    php

--- php-mvc-blog/views/layouts/default/footer.php
+++ php-mvc-blog/views/layouts/default/footer.php
@@ -2,13 +2,12 @@
 
         <footer class="footer">
                 <div class="panel-footer footer">
-                    <p class="text-center">&copy; Forum System MVC 2015</p>
+                    <p class="text-center">&copy; AniSoft Forum, 2015</p>
                 </div>
         </footer>
     </div>
 
     <script src="/library/jquery-2.1.1.js"></script>
-    <script src="/library/bootstrap/bootstrap.min.js"></script>
     <script src="/library/bootstrap/bootstrap.min.js"></script>
     <script src="/content/js.js"></script>
 </body>
